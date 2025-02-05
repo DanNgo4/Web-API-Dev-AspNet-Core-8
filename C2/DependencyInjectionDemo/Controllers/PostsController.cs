b735e65f4862fef681b9f185b3f565160243b89d
Dan Ngo <danngo.au@gmail.com>
2025-02-06 10:43:39 +1100

    up to page 71 slide 94

---
+++ C2/DependencyInjectionDemo/Controllers/PostsController.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Mvc;
+using DependencyInjectionDemo.Models;
+using DependencyInjectionDemo.Services;
+
+namespace DependencyInjectionDemo.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PostsController : ControllerBase
+{
+    private readonly PostsService _postsService;
+
+    public PostsController()
+    {
+        _postsService = new PostsService();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Post>>> GetPosts()
+    {
+        var posts = await _postsService.GetAllPosts();
+        return Ok(posts);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Post>> GetPost(int id)
+    {
+        var post = await _postsService.GetPost(id);
+        if (post == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(post);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Post>> CreatePost(Post post)
+    {
+        await _postsService.CreatePost(post);
+
+        // Retuns a response message with the specified action name, route values and post.
+        // Call the GetPost() action/method to return the newly created post
+        return CreatedAtAction(nameof(GetPost), new { id = post.Id }, post);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdatePost(int id, Post post)
+    {
+        if (id != post.Id)
+        {
+            return BadRequest();
+        }
+
+        var updatedPost = await _postsService.UpdatePost(id, post);
+        if (updatedPost == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(post);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeletePost(int id)
+    {
+        var post = await _postsService.GetPost(id);
+        if (post == null) return NotFound();
+
+        await _postsService.DeletePost(id);
+        return Ok();
+    }
+}
