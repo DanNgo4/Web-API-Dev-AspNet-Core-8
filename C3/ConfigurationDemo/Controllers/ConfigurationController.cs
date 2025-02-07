bd7af898dc01ba88a5ed28881cd55cc4b9a7d8f7
Dan Ngo <danngo.au@gmail.com>
2025-02-07 18:47:23 +1100

    up to page 104 slide 127

---
+++ C3/ConfigurationDemo/Controllers/ConfigurationController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using ConfigurationDemo.Models;
+
+namespace ConfigurationDemo.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ConfigurationController(IConfiguration configuration) : ControllerBase
+{
+    [HttpGet]
+    [Route("my-key")]
+    public ActionResult GetMyKey()
+    {
+        var myKey = configuration["MyKey"];
+        return Ok(myKey);
+    }
+
+    [HttpGet]
+    [Route("database-configuration")]
+    public ActionResult GetDatabaseConfiguration()
+    {
+        var type = configuration["Database:Type"];
+        var connectionString = configuration["Database:ConnectionString"];
+        return Ok(new { Type = type, ConnectionString = connectionString });
+    }
+
+    [HttpGet]
+    [Route("database-configuration-with-bind")]
+    public ActionResult GetDatabaseConfigurationWithBind()
+    {
+        var databaseOption = new DatabaseOption();
+
+        // The SectionName is defined in the DatabaseOption class, which shows the section name in the appsettings.json file
+        configuration.GetSection(DatabaseOption.SectionName).Bind(databaseOption);
+
+        // alternative implementation:
+        // configuration.Bind(DatabaseOption.SectionName, databaseOption);
+
+        return Ok(new { databaseOption.Type, databaseOption.ConnectionString });
+    }
+}
---
+++ C3/ConfigurationDemo/Models/DatabaseOption.cs
@@ -0,0 +1,9 @@
+namespace ConfigurationDemo.Models;
+
+public class DatabaseOption
+{
+    public const string SectionName = "Database";
+    
+    public string Type             { get; set; } = string.Empty;
+    public string ConnectionString { get; set; } = string.Empty;
+}
