73e3ba0358a07c2ef788323cc1c9c9e87e0c3791
Dan Ngo <danngo.au@gmail.com>
2025-07-25 18:02:30 +1000

    up to page 258 slide 281

---
+++ C7/EfCoreDemo/k6/script.js
@@ -0,0 +1,12 @@
+﻿import http from "k6/http";
+import { sleep } from "k6";
+
+export const options = {
+    vus: 500,
+    duration: "30s"
+}
+
+export default function () {
+    http.get("http://localhost:5068/api/Invoices?page=1&pageSize=10");
+    sleep(1);
+}
