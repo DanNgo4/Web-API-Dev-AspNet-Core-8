0564eabb2aa2e308c1116fd2cd58c66a7d619f47
Dan Ngo <danngo.au@gmail.com>
2025-11-04 21:57:47 +1100

    up to page 276 slide 299

---
+++ C8/AuthenticationDemo/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using AuthenticationDemo.Models.Authentication;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationDemo.Data;
+
+public class AppDbContext 
+           : IdentityDbContext<AppUser>
+{
+    private readonly IConfiguration _configuration;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options, IConfiguration configuration) 
+         : base(options)
+    {
+        _configuration = configuration;
+    }
+           
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+    }
+}
--- C8/AuthenticationDemo/Models/Authentication/AppUser.cs
+++ C8/AuthenticationDemo/Models/Authentication/AppUser.cs
@@ -4,4 +4,7 @@ namespace AuthenticationDemo.Models.Authentication;
 
 public class AppUser : IdentityUser
 {
+    public string FirstName      { get; set; }
+    public string LastName       { get; set; }
+    public string ProfilePicture { get; set; }
 }
